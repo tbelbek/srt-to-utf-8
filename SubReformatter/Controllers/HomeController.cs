9a7974445b46d2dd1d03caea3312ed355beadcd0
tughan <Ttughanbelbek@gmail.com>
2018-05-10 09:31:36 +0300

    added already converted check

--- SubReformatter/Controllers/HomeController.cs
+++ SubReformatter/Controllers/HomeController.cs
@@ -21,13 +21,17 @@ namespace SubReformatter.Controllers
 
         public static List<string> ConvertFiles()
         {
-            var path = ConfigurationManager.AppSettings["srtPath"];
-            var myFiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(s => s.Contains(".srt"));
-            AnsiConvert(myFiles);
-            return myFiles.ToList();
+            var pathList = ConfigurationManager.AppSettings["srtPath"].Split(',').ToList();
+            List<string> srtFiles = new List<string>();
+            foreach (var path in pathList)
+            {
+                srtFiles.AddRange(Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(s => s.Contains(".srt")));
+            }
+            AnsiConvert(srtFiles);
+            return srtFiles.ToList();
         }
 
-        public static void  AnsiConvert(IEnumerable<string> fileList)
+        public static void AnsiConvert(IEnumerable<string> fileList)
         {
             Parallel.ForEach(fileList, f => { ConvertFileEncoding(f, Encoding.UTF8); });
         }
@@ -43,6 +47,8 @@ namespace SubReformatter.Controllers
         {
             try
             {
+                if (Equals(GetEncoding(path), Encoding.UTF8)) return;
+
                 Encoding fileEncode = Encoding.GetEncoding("ISO-8859-9");
 
                 string stt = System.IO.File.ReadAllText(path, fileEncode);
@@ -64,6 +70,23 @@ namespace SubReformatter.Controllers
 
         }
 
+        public static Encoding GetEncoding(string filename)
+        {
+            // Read the BOM
+            var bom = new byte[4];
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                file.Read(bom, 0, 4);
+            }
+
+            // Analyze the BOM
+            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
+            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
+            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
+            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
+            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return Encoding.UTF32;
+            return Encoding.ASCII;
+        }
 
         public ActionResult About()
         {
